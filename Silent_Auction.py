82e5b9b9646cd05d2e06de50e41a5d684aa99c1a
francis1102 <83624941+francis1102@users.noreply.github.com>
2022-03-03 10:06:21 +1300

    update

--- Silent_Auction.py
+++ Silent_Auction.py
@@ -3,14 +3,26 @@ Created by Francis Torcuato
 24/02/2022
 """
 
-def auction_rotation():
+
 # Main Routine
 item = input("What is the auction for? ")
 reserve_price = float(input("What is the reserve price? "))
 print(f"The auction for the {item} has started: ")
 highest_bid = 0
-enter_bid = input("What is your bid? ")
+enter_bid = 0
 while enter_bid != -1:
-enter_bid = input("What is your bid? ")
+    enter_bid = float(input("What is your bid? "))
+    if highest_bid > enter_bid:
+        print("Please enter the highest bid: ")
+        print(f"highest bid so far is ${highest_bid}")
+    else:
+        print(f"Highest bid so far is ${enter_bid}")
+        highest_bid = enter_bid
+
+if highest_bid >= reserve_price:
+    print(f"The auction for the {item} finished with a bid of ${highest_bid}")
+else:
+    print(f"The {item} didn't sell ")
+
 
 
